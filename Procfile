45351d59a2ee2ea9e6c340009998039d28989695
Tony Adriansen <tony.adriansen@gmail.com>
2023-06-27 22:19:49 -0500

    --amend

--- Procfile
+++ Procfile
@@ -1,2 +1,3 @@
 release: python backend/src/manage.py migrate
 web: gunicorn exercise_log.wsgi --log-file - --chdir backend/src
+worker: celery -A exercise_log worker --loglevel=info
--- backend/src/exercise_log/celery.py
+++ backend/src/exercise_log/celery.py
@@ -5,6 +5,8 @@ from datetime import timedelta
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'exercise_log.settings')
 
 app = Celery('exercise_log')
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULTS_BACKEND=os.environ['REDIS_URL'])
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
